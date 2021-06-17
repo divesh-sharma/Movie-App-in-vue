ee3d57a5ccdcb472aa575a198734ed5bd3f3483c
divesh.sharma_Angular <divesh.sharma@appinventiv.com>
2021-06-17 08:43:04 +0530

    first commit

---
+++ src/components/MovieForm.vue
@@ -0,0 +1,50 @@
+<template>
+  <form @submit.prevent="onSubmit" class="text-center">
+    <input
+      class="form-control mb-2"
+      placeholder="Awesome Movie Name"
+      name="name"
+      v-model="name"
+      type="text"
+    />
+    <input
+      type="text"
+      class="form-control mb-2"
+      placeholder="url"
+      name="url"
+      v-model="url"
+    />
+    <button class="btn btn-info btn-block" type="submit">Add Movie</button>
+  </form>
+</template>
+
+<script>
+import { v4 as uuidv4 } from "uuid";
+export default {
+  name: "Form",
+  props: {
+    addMovie: Function,
+  },
+  data() {
+    return {
+      name: "",
+      url: "",
+    };
+  },
+  methods: {
+    onSubmit: function () {
+      this.addMovie({
+        name: this.name,
+        url: this.url,
+        id: uuidv4(),
+        date: Date.now(),
+      });
+      this.name = "";
+      this.url = "";
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/layout/header.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="navbar navbar-dark bg-primary">
+    <a href="#" class="navbar-brand">
+      <img src="../assets/logo-white.png" alt="Logo" width="30" height="30" />
+    </a>
+    <span class="navbar-text text-white"> {{ title }} </span>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: ["title"],
+};
+</script>
+
+<style>
+</style>
